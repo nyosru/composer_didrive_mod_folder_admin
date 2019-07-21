7625958552a142fbcf7d3ff8b4a71597d42969ae
Сергей <nyos@rambler.ru>
2019-07-21 09:29:45 +0500

    no message

--- class.php
+++ class.php
@@ -4,19 +4,19 @@
 класс модуля
 **/
 
-namespace Nyos\mod;
-
-if (!defined('IN_NYOS_PROJECT'))
-    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
-
-class mod_class {
-
-    public static $dir_img_server = false;
-
-    public static function creatFolderImage($folder) {
-        /**
-        
-        **/
-    }
-
-}
+//namespace Nyos\mod;
+//
+//if (!defined('IN_NYOS_PROJECT'))
+//    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
+//
+//class mod_class {
+//
+//    public static $dir_img_server = false;
+//
+//    public static function creatFolderImage($folder) {
+//        /**
+//        
+//        **/
+//    }
+//
+//}
